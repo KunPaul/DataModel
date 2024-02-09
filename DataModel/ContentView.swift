1d42a4e7f49004ff91a428054d39345f1a6efe01
KunPaul <kungurtsev200@mail.ru>
2024-02-09 17:38:54 +0500

    Initial Commit

---
+++ DataModel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataModel
+//
+//  Created by Павел Кунгурцев on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataModel/DataModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataModelApp.swift
+//  DataModel
+//
+//  Created by Павел Кунгурцев on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DataModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
